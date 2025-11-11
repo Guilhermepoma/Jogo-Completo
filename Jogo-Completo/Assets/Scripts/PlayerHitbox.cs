6356d61bdabaa540bf204e1db84528c0037f5905
Guilherme_Poma_Cercena <guilhermepomacercena@gmail.com>
2025-11-11 16:01:29 -0300

    recomeço

---
+++ Jogo-Completo/Assets/Scripts/PlayerHitbox.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerHitbox : MonoBehaviour
+{
+    private bool ativa = false;
+
+    public void AtivarHitbox()
+    {
+        ativa = true;
+        gameObject.SetActive(true);
+        Invoke(nameof(DesativarHitbox), 0.2f); // hitbox dura 0.2 segundos
+    }
+
+    void DesativarHitbox()
+    {
+        ativa = false;
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!ativa) return;
+
+        if (collision.CompareTag("Inimigo"))
+        {
+            Debug.Log("Inimigo atingido e destruído!");
+            Destroy(collision.gameObject); // 💥 inimigo some na hora
+        }
+    }
+}
